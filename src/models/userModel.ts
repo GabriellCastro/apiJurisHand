d6b02c1d8e78bcc04463a43e593e8b54f5d81e3a
GabriellCastro <kastro.gabriel17@gmail.com>
2022-02-17 00:54:53 -0400

    add: create models ajustes 2 🦎

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,7 +4,7 @@ import { ObjectId } from 'mongodb';
 export const getByid = async (id: any) => {
   if(!ObjectId.isValid(id)) return null;
   const db = await connection();
-  return db.collection('users').findOne(ObjectId(id));
+  return db.collection('users').findOne(new ObjectId(id));
 }
 
 export const add = async ({ nome, email, password, hora_de_criacao, hora_de_atualizacao }) => {
@@ -17,11 +17,11 @@ export const update = async ({ id, nome, email, password }) => {
   if(!ObjectId.isValid(id)) return null;
   const db = await connection();
   return db.collection('users')
-    .updateOne({ _id: ObjectId(id) }, { $set: { nome, email, password } });
+    .updateOne({ _id: new ObjectId(id) }, { $set: { nome, email, password } });
 }
 
 export const exclude = (id: any) => {
   if(!ObjectId.isValid(id)) return null;
-  connection().then((db) => db.collection('users').deleteOne({ _id: ObjectId(id) }))
+  connection().then((db) => db.collection('users').deleteOne({ _id: new ObjectId(id) }))
 }
 
