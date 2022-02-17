aeba61296582fa3709a41a33154edd6d1dbd7c09
GabriellCastro <kastro.gabriel17@gmail.com>
2022-02-17 01:17:18 -0400

    add: Index Router Config 🦎

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import * as express from 'express';
 import * as bodyParser from "body-parser";
-import 
+import { userController } from './controllers/userController';
+
+const app: express.Application = express();
+
+app.use(bodyParser.json());
+
+app.use('/user', userController);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
