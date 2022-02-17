4052bbf5b38c6a9f80179313d11feb88059a5953
GabriellCastro <kastro.gabriel17@gmail.com>
2022-02-17 00:52:18 -0400

    add: create models ajustes 🦎

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,28 +1,27 @@
 import connection from './connection';
-const { ObjectId } = require('mongodb')
+import { ObjectId } from 'mongodb';
 
-const getByid = async (id: any) => {
+export const getByid = async (id: any) => {
   if(!ObjectId.isValid(id)) return null;
   const db = await connection();
   return db.collection('users').findOne(ObjectId(id));
 }
 
-const add = async ({ nome, email, password, hora_de_criacao, hora_de_atualizacao }) => {
+export const add = async ({ nome, email, password, hora_de_criacao, hora_de_atualizacao }) => {
   const db = await connection();
   return db.collection('users')
     .insertOne({ nome, email, password, hora_de_criacao, hora_de_atualizacao });
 }
 
-const update = async ({ id, nome, email, password }) => {
+export const update = async ({ id, nome, email, password }) => {
   if(!ObjectId.isValid(id)) return null;
   const db = await connection();
   return db.collection('users')
     .updateOne({ _id: ObjectId(id) }, { $set: { nome, email, password } });
 }
 
-const exclude = (id: any) => {
+export const exclude = (id: any) => {
   if(!ObjectId.isValid(id)) return null;
   connection().then((db) => db.collection('users').deleteOne({ _id: ObjectId(id) }))
 }
 
-module.exports = { getByid, add, update, exclude }
